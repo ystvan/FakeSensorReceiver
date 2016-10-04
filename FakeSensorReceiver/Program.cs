d467686cd7af669eb680528f36985dceff808da3
ystvan <istvan.marki@gmail.com>
2016-10-04 10:10:19 +0200

    fill me in
    some noob magic

--- FakeSensorReceiver/Program.cs
+++ FakeSensorReceiver/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,50 @@ namespace FakeSensorReceiver
     {
         static void Main(string[] args)
         {
+            Console.Title = "UDP CLIENT";
+            Start();
+        }
+
+        static void Start()
+        {
+
+            // Listening to a port (port number must be identical to server)
+            UdpClient simpleSocket = new UdpClient(7080);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            //Go to an infinite loop
+
+            while (true)
+            {
+                Byte[] databuffer = new byte[4000];
+
+                databuffer = simpleSocket.Receive(ref RemoteIpEndPoint);
+
+                String text = Encoding.ASCII.GetString(databuffer);
+
+                string[] data = text.Split(' ');
+                string clientName = data[0];
+                string text1 = data[1] + data[2];
+
+                int light;
+                int temp;
+                String timestamp;
+
+                TextReader tr = new StringReader(text);
+                String str1 = tr.ReadLine();
+                String str2 = tr.ReadLine();
+                String str3 = tr.ReadLine();
+
+                light = Convert.ToInt32(str1.Split(' ')[1]);
+                temp = Convert.ToInt32(str2.Split(' ')[1]);
+
+                timestamp = str3.Split(' ')[1];
+
+                Console.WriteLine(text);
+
+
+            }
         }
     }
 }
